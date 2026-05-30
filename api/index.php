0bf15b68aee410fc03a025101552f2ef273b2652
Arya fawwaz <aryaseptyan41@gmail.com>
2026-05-31 01:28:26 +0700

    Intercept internal exceptions after request handling

--- api/index.php
+++ api/index.php
@@ -6,6 +6,12 @@ error_reporting(E_ALL);
 
 try {
     require __DIR__ . '/../public/index.php';
+    if (file_exists('/tmp/laravel-error.txt')) {
+        http_response_code(200);
+        echo "<h1>Laravel Captured Exception:</h1>";
+        echo "<pre>" . htmlspecialchars(file_get_contents('/tmp/laravel-error.txt')) . "</pre>";
+        @unlink('/tmp/laravel-error.txt');
+    }
 } catch (\Throwable $e) {
     http_response_code(200);
     echo "<h1>Vercel Serverless Exception</h1>";
