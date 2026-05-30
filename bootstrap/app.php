ea88bd2166b170f2b0d929749b27f78ddbb72182
Arya fawwaz <aryaseptyan41@gmail.com>
2026-05-31 00:54:31 +0700

    Configure Vercel deployment and dynamic storage path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -20,3 +20,26 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->create();
+
+if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
+    $storagePath = '/tmp/storage';
+    $folders = [
+        $storagePath,
+        $storagePath . '/app',
+        $storagePath . '/app/public',
+        $storagePath . '/framework',
+        $storagePath . '/framework/cache',
+        $storagePath . '/framework/cache/data',
+        $storagePath . '/framework/sessions',
+        $storagePath . '/framework/views',
+        $storagePath . '/logs',
+    ];
+    foreach ($folders as $folder) {
+        if (!is_dir($folder)) {
+            mkdir($folder, 0777, true);
+        }
+    }
+    $app->useStoragePath($storagePath);
+}
+
+return $app;
